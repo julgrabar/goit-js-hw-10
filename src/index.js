aa65fe9e1ffb6e2792132a52dbc22dd979b6c53e
julgrabar <45499113+julgrabar@users.noreply.github.com>
2022-01-13 00:35:48 +0200

    Решение

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
 import './css/styles.css';
-import './js/fetchCountries.js';
+import fetchCountries from './js/fetchCountries.js';
+import debounce from 'lodash/debounce';
 
 const DEBOUNCE_DELAY = 300;
+
+const refs={
+    input: document.querySelector('#search-box'),
+    countriesList: document.querySelector(".country-list"),
+    countriesInfo: document.querySelector(".country-info")
+}
+refs.input.addEventListener('input', debounce(onInput,DEBOUNCE_DELAY));
+function onInput(e){
+    refs.countriesInfo.innerHTML="";
+    refs.countriesList.innerHTML="";
+    let value= e.target.value.trim();
+    if(value===""){
+        return;
+    } ;
+    fetchCountries(value)  ;
+}
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,23 +1,10 @@
-import debounce from 'lodash/debounce';
 import Notiflix from 'notiflix';
-const refs={
-    input: document.querySelector('#search-box'),
-    countriesList: document.querySelector(".country-list"),
-    countriesInfo: document.querySelector(".country-info")
-}
-refs.input.addEventListener('input', debounce(onInput,300));
-function onInput(e){
-    refs.countriesInfo.innerHTML="";
-    refs.countriesList.innerHTML="";
-    let value= e.target.value.trim();
-    if(value===""){
-        return;
-    }
+export default function fetchCountries(value){
     fetch(`https://restcountries.com/v3.1/name/${value}?fields=name,capital,population,flags,languages`)
     .then(responce=>{return responce.json()})
     .then(countries=>{
         if(countries.length===1){
-            return refs.countriesInfo.innerHTML=`<div class="head">
+            return document.querySelector(".country-info").innerHTML=`<div class="head">
             <img src="${countries[0].flags.svg}" height="20">
             <p>${countries[0].name.common}</p>
             </div>
@@ -34,11 +21,10 @@ function onInput(e){
             </li>`
         })
         .join('');
-        refs.countriesList.innerHTML=markup;
+        document.querySelector(".country-list").innerHTML=markup;
+        
     }
     )
     .catch(error=>{
         Notiflix.Notify.failure("Oops, there is no country with that name")
-    });
-    
-}
+    });}
